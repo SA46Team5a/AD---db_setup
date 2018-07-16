a47effadb53048ec460fc9bbccb7ae58234d6fc7
Divyashree <divyashree.ga@gmail.com>
2018-07-16 15:43:28 +0800

    merged all table scripts

--- CreateDB.sql
+++ CreateDB.sql
@@ -83,6 +83,137 @@ create table OrderSupplierDetails (
 );
 go
 
+create table CollectionPoint (
+	CollectionPointID	int		not null,	
+	CollectionPointName	nvarchar(64)	not null,
+	CollectionPointTime Time(0) not null,
+	primary key (CollectionPointID)
+);
+go
+
+create table Departments (
+	DepartmentID	nvarchar(4)		not null,
+	DepartmentName	nvarchar(20)	not null,
+	DepartmentHeadID	nvarchar(20)		not null,
+	CollectionPointID	int	not null,
+	primary key (DepartmentID),
+	foreign key (CollectionPointID) references CollectionPoint(CollectionPointID)
+);
+go
+
+create table Employees (
+	EmployeeID	nvarchar(20)		not null,
+	DepartmentID	nvarchar(4)		not null,
+	EmployeeName	nvarchar(20)	not null,
+	PhoneNumber	nvarchar(20)		not null,
+	EmailID	nvarchar(40)		not null,
+	Sex	nvarchar(6)		not null,	
+	primary key (EmployeeID	),
+	foreign key (DepartmentID) references Departments(DepartmentID)
+);
+go
+
+
+Alter table Departments 
+ADD FOREIGN KEY (DepartmentHeadID) REFERENCES Employees(EmployeeID);
+
+
+create table Authority (
+	AuthorityID	int		not null  identity(1,1),
+	EmployeeID	nvarchar(20)		not null,
+	StartDate	Datetime	not null,
+	EndDate	DateTime		not null,	
+	primary key (AuthorityID),
+	foreign key (EmployeeID) references Employees(EmployeeID)
+);
+go
+
+create table DepartmentRepresentative (
+	DeptRepID	int		not null  identity(1,1),
+	EmployeeID	nvarchar(20)		not null,
+	StartDate	Datetime	not null,
+	EndDate	DateTime		not null,	
+	primary key (DeptRepID),
+	foreign key (EmployeeID) references Employees(EmployeeID)
+);
+go
+
+
+create table RetreivalStatus (
+	RetreivalStatusID	int		not null,
+	RetrevialStatusName	nvarchar(20)		not null,		
+	primary key (RetreivalStatusID)	
+);
+go
+
+create table ApprovalStatus (
+	ApprovalStatusID	int		not null,
+	ApprovalStatusName	nvarchar(20)		not null,		
+	primary key (ApprovalStatusID)	
+);
+go
+
+
+create table Requisition (
+	RequisitionID	int		not null  identity(1,1),
+	EmployeeID	nvarchar(20)		not null,
+	RequestedDate	Datetime	not null,
+	AuthorityID	int		not null,
+	ApproveDate Datetime not null,
+	RetreivalStatusID int		not null,
+	ApprovalStatusID	int		not null,		
+	primary key (RequisitionID),
+	foreign key (EmployeeID) references Employees(EmployeeID),
+	foreign key (AuthorityID) references Authority(AuthorityID),
+	foreign key (RetreivalStatusID) references RetreivalStatus(RetreivalStatusID),
+	foreign key (ApprovalStatusID) references ApprovalStatus(ApprovalStatusID)
+
+);
+go
+
+create table RequisitionDetails (
+	RequisitionDetailsID		int		not null	identity(1,1),
+	RequisitionID			int		not null,
+	ItemID				int		not null,
+	Quantity 			int		not null,	
+	primary key (RequisitionDetailsID),
+	foreign key(RequisitionID) references Requisition(RequisitionID),
+	foreign key(ItemID) references Items(ItemID)
+);
+go
+
+
+create table Disbursement (
+	DisbursementID		int		not null	identity(1,1),
+	EmployeeID			nvarchar(20)	not null,
+	DisbursementDate	Datetime		not null,
+	Passcode 			nvarchar(4)	not null,	
+	RequisitionID			int		not null,
+	CollectedBy int not null, 
+	primary key (DisbursementID),
+    foreign key(EmployeeID) references Employees(EmployeeID),
+	foreign key(RequisitionID) references Requisition(RequisitionID),
+	foreign key(CollectedBy) references DepartmentRepresentative(DeptRepID)
+);
+go
+
+
+create table DisbursementDetails (
+	DisbursementDetailsID		int		not null	identity(1,1),
+	ItemID			int	not null,
+	DisbursementID	int		not null,
+	Quantity  int		not null,
+	CollectedQty int not null,	
+	Reason nvarchar(40)	not null,
+	 RequisitionDetailsID int not null,
+	primary key (DisbursementDetailsID),
+    foreign key(ItemID) references Items(ItemID),
+	foreign key(DisbursementID) references Disbursement(DisbursementID),
+	foreign key(RequisitionDetailsID) references RequisitionDetails(RequisitionDetailsID)
+);
+go
+
+
 create table StockTransaction (
 	TransactionID		int		not null	identity(1,1),
 	ItemID			int		not null,

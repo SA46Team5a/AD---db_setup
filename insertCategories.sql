3c8dddb018761bd0da4b3cb2c43cf74f525bbd84
pav <Pavana26@github.com>
2018-07-16 17:11:49 +0800

    insert values to category

---
+++ insertCategories.sql
@@ -0,0 +1,76 @@
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Clip');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Envelope');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Eraser');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Exercise');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('File');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Pen');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Puncher');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Pad');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Paper');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Ruler');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Scissors');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Tape');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Sharpener');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Shorthand');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Stapler');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Tacks');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Tparency');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('Tray');
+/* INSERT QUERY */
+INSERT INTO Categories(CategoryName) 
+VALUES 
+  ('');

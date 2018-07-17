cfe5dcec973159c96efcc57c71c57965cfa539e5
pav <Pavana26@github.com>
2018-07-17 21:39:13 +0800

    populate stock transaction

---
+++ insertStockTransaction.sql
@@ -0,0 +1,529 @@
+use StationeryStore;
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C001', 'Stock received from the supplier', 
+    150, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C002', 'Stock received from the supplier', 
+    500, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C003', 'Stock received from the supplier', 
+    500, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C004', 'Stock received from the supplier', 
+    500, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C005', 'Stock received from the supplier', 
+    450, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C006', 'Stock received from the supplier', 
+    450, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E001', 'Stock received from the supplier', 
+    450, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E002', 'Stock received from the supplier', 
+    450, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E003', 'Stock received from the supplier', 
+    450, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E004', 'Stock received from the supplier', 
+    450, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E005', 'Stock received from the supplier', 
+    450, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E006', 'Stock received from the supplier', 
+    450, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E007', 'Stock received from the supplier', 
+    450, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E008', 'Stock received from the supplier', 
+    450, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E020', 'Stock received from the supplier', 
+    450, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E021', 'Stock received from the supplier', 
+    450, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E030', 'Stock received from the supplier', 
+    450, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E031', 'Stock received from the supplier', 
+    450, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E032', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'E033', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'F020', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'F021', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'F022', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'H011', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'H012', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'H013', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'H030', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'H031', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'H032', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'P010', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'P011', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'P012', 'Stock received from the supplier', 
+    250, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'P020', 'Stock received from the supplier', 
+    250, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'P021', 'Stock received from the supplier', 
+    250, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'P030', 'Stock received from the supplier', 
+    250, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'P031', 'Stock received from the supplier', 
+    250, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'P032', 'Stock received from the supplier', 
+    300, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'R001', 'Stock received from the supplier', 
+    300, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'R002', 'Stock received from the supplier', 
+    300, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'S011', 'Stock received from the supplier', 
+    300, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'S012', 'Stock received from the supplier', 
+    300, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'S021', 'Stock received from the supplier', 
+    300, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'S022', 'Stock received from the supplier', 
+    300, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'S023', 'Stock received from the supplier', 
+    300, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'S040', 'Stock received from the supplier', 
+    300, 'E013'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'S041', 'Stock received from the supplier', 
+    300, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'S100', 'Stock received from the supplier', 
+    300, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'T001', 'Stock received from the supplier', 
+    300, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'T002', 'Stock received from the supplier', 
+    300, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'T003', 'Stock received from the supplier', 
+    300, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C001', 'Damaged goods',-20, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C002', 'Broken goods',-30, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C003', 'Missing from the rack', 
+    -10, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C004', 'Broken goods',-10, 'E001'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C005', 'Damaged goods',-5, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'C006', 'Burnt and damaged',-4, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  ('T001', 'Stolen',-20, 'E012');
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'T002', 'Free samples received', 
+    10, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  (
+    'T003', 'Complimentary gift', 5, 'E012'
+  );
+/* INSERT QUERY */
+INSERT INTO StockTransaction(
+  ItemID, Description, Adjustment, EmployeeID
+) 
+VALUES 
+  ('');

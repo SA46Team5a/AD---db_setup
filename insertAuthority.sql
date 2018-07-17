3cadc721475c9943ed37052bf1040368bbbbcc02
Divyashree <divyashree.ga@gmail.com>
2018-07-17 18:49:29 +0800

    added insertAuthority and insertDepartment.sql

---
+++ insertAuthority.sql
@@ -0,0 +1,33 @@
+
+Use StationeryStore
+
+;
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E025', '2016-05-06', '2016-10-06'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E026', '2016-05-20', '2016-05-30'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E025', '2017-01-30', '2017-02-09'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E025', '2017-05-09', '2017-05-21'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E026', '2018-06-10', '2016-06-20'
+  );
---
+++ insertDeprartmentRepresentative.sql
@@ -0,0 +1,56 @@
+use StationeryStore;
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E016', '2018-01-01', '2019-01-01'
+  );
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E017', '2018-01-01', '2019-01-01'
+  );
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E018', '2018-01-01', '2019-01-01'
+  );
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E019', '2018-01-01', '2019-01-01'
+  );
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E020', '2018-01-01', '2019-01-01'
+  );
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E021', '2018-01-01', '2019-01-01'
+  );
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E022', '2018-01-01', '2019-01-01'
+  );
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E023', '2018-01-01', '2019-01-01'
+  );
+/* INSERT QUERY */
+INSERT INTO DepartmentRepresentative(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E024', '2018-01-01', '2019-01-01'
+  );
+

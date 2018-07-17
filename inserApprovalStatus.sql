04c481f10ff6f82f920df1d7829a1c4fd629a68f
Divyashree <divyashree.ga@gmail.com>
2018-07-17 22:32:50 +0800

    added insert approval,requisition,retrievalStatus.sql

---
+++ inserApprovalStatus.sql
@@ -0,0 +1,25 @@
+use StationeryStore;
+/* INSERT QUERY */
+INSERT INTO ApprovalStatus(
+  ApprovalStatusID, ApprovalStatusName
+) 
+VALUES 
+  (1, 'Unsubmitted');
+/* INSERT QUERY */
+INSERT INTO ApprovalStatus(
+  ApprovalStatusID, ApprovalStatusName
+) 
+VALUES 
+  (2, 'Pending');
+/* INSERT QUERY */
+INSERT INTO ApprovalStatus(
+  ApprovalStatusID, ApprovalStatusName
+) 
+VALUES 
+  (3, 'Approved');
+/* INSERT QUERY */
+INSERT INTO ApprovalStatus(
+  ApprovalStatusID, ApprovalStatusName
+) 
+VALUES 
+  (4, 'Deleted');
---
+++ insertRequisition.sql
@@ -0,0 +1,22 @@
+use StationeryStore;/* INSERT QUERY */
+INSERT INTO Requisition(
+  EmployeeID, RequestedDate, AuthorityID, 
+  ApproveDate, RetrievalStatusID,
+  ApprovalStatusID
+) 
+VALUES 
+  (
+    'E025', '2016-05-08', 1, '2016-08-05', 
+    4, 4
+  );
+/* INSERT QUERY */
+INSERT INTO Requisition(
+  EmployeeID, RequestedDate, AuthorityID, 
+  ApproveDate, RetrievalStatusID, 
+  ApprovalStatusID
+) 
+VALUES 
+  (
+    'E026', '2018-06-12', 5, '2018-06-14', 
+    4, 3
+  );

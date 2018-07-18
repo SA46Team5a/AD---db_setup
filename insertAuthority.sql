2d2dd6fca45713a9677038cd983981d68a5811d3
Jack <jsim89@gmail.com>
2018-07-18 11:25:09 +0800

    changed authority insert statements

--- insertAuthority.sql
+++ insertAuthority.sql
@@ -1,31 +1,97 @@
 
 Use StationeryStore;
+/* INSERT QUERY */
 INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
 VALUES 
   (
-    'E025', '2016-05-06', '2016-10-06'
+    'E026', '2016-05-06', '2016-10-06'
   );
 /* INSERT QUERY */
 INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
 VALUES 
   (
-    'E026', '2016-05-20', '2016-05-30'
+    'E035', '2016-10-07', '2016-10-12'
   );
 /* INSERT QUERY */
 INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
 VALUES 
   (
-    'E025', '2017-01-30', '2017-02-09'
+    'E026', '2016-10-13', '2016-11-01'
   );
 /* INSERT QUERY */
 INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
 VALUES 
   (
-    'E025', '2017-05-09', '2017-05-21'
+    'E008', '2016-11-02', '2017-03-31'
   );
 /* INSERT QUERY */
 INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
 VALUES 
   (
-    'E026', '2018-06-10', '2016-06-20'
+    'E026', '2017-04-01', '2017-04-16'
   );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E035', '2017-04-17', '2017-04-28'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E026', '2017-04-29', '2017-05-10'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E008', '2017-05-11', '2017-10-07'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E026', '2017-10-08', '2017-10-24'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E035', '2017-10-25', '2017-11-13'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E026', '2017-11-14', '2017-11-29'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E008', '2017-11-30', '2018-03-09'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E026', '2018-03-10', '2018-03-21'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E035', '2018-03-22', '2018-03-25'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  (
+    'E026', '2018-03-26', '2018-04-10'
+  );
+/* INSERT QUERY */
+INSERT INTO Authority(EmployeeID, StartDate, EndDate) 
+VALUES 
+  ('E008', '2018-04-11', '');
+GO
